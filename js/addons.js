664a55268bd0a3c3025b5624e78afd4b9ee19e64
Brihat Ratna Bajracharya <brihat.rb@gmail.com>
2023-07-12 07:32:17 +0545

    Update addons.js

--- js/addons.js
+++ js/addons.js
@@ -3,25 +3,27 @@ function scroll_to_today() {
 
     var today = new Date();
     var today_date_list = convert_ad_to_bs(today.getFullYear(), today.getMonth() + 1, today.getDate()).split(" ");
-    var today_mm_dd = today_date_list[1].padStart(2, "0") + "-" + today_date_list[2].padStart(2, "0");
-    var today_mm_dd_2 = today_mm_dd;
-    var today_element = document.getElementById(today_mm_dd);
-
-    if(view == 4) {
-        today_mm_dd = (today.getMonth() + 1).toString().padStart(2, "0") + "-" + today.getDate().toString().padStart(2, "0") + "_EN";
-        today_mm_dd_2 = today_mm_dd.replace("_EN", "_NP");
-    }
-    else if (view !=2 ) {
-        today_mm_dd += "_NP";
-        today_mm_dd_2 = today_mm_dd.replace("_NP", "_EN");
-    }
-    else if (view == 2 || view == 3) {
+    
+    if (view == 2 || view == 3) {
         if(document.getElementById("year").value != today_date_list[0]) {
             return;
         }
     }
-    else {
-        console.warn("Some other case occured.");
+
+    var today_mm_dd = today_date_list[1].padStart(2, "0") + "-" + today_date_list[2].padStart(2, "0");
+    var today_mm_dd_2 = today_mm_dd;
+    var today_element = document.getElementById(today_mm_dd);
+
+    if(view == 4) { // INTERNATIONAL EVENTS CASE
+        today_mm_dd = (today.getMonth() + 1).toString().padStart(2, "0") + "-" + today.getDate().toString().padStart(2, "0") + "_EN";
+        today_mm_dd_2 = today_mm_dd.replace("_EN", "_NP");
+    }
+    else if (view != 2 ) { // NATIONAL EVENTS AND PUBLIC HOLIDAY EVENTS CASE
+        today_mm_dd += "_NP";
+        today_mm_dd_2 = today_mm_dd.replace("_NP", "_EN");
+    }
+    else { // LUNAR EVENTS CASE AND OTHER ...
+        
     }
 
     today_element = document.getElementById(today_mm_dd);
