24bf6a2d35a6e614e071a815da81593c800f7275
Brihat Ratna Bajracharya <brihat.rb@gmail.com>
2024-04-13 07:38:49 +0545

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,3 @@
 // const AD_MONTHS_ENG = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const DEFAULT_YEAR = 2080;
+const DEFAULT_YEAR = 2081;
